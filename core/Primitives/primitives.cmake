706b8f55ce808ac58f84e33816ee6b25c64a8979
Solomon Ionut <i.solomon18@hotmail.com>
2025-02-16 02:21:19 +0200

    FEAT: fix spelling

--- core/Primitives/primitives.cmake
+++ core/Primitives/primitives.cmake
@@ -1,6 +1,6 @@
-set(PRIMITVES_BASE_DIR ${CORE_BASE_DIR}/primitives)
+set(PRIMITIVES_BASE_DIR ${CORE_BASE_DIR}/primitives)
 
 set(PRIMITIVES_SRC
-    ${PRIMITVES_BASE_DIR}/Pixel/Pixel.cpp
-    ${PRIMITVES_BASE_DIR}/Circle/Circle.cpp
+    ${PRIMITIVES_BASE_DIR}/Pixel/Pixel.cpp
+    ${PRIMITIVES_BASE_DIR}/Circle/Circle.cpp
 )
