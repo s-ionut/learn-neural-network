f555a2ce5a94ade66c09e0164d9633e3fa2599bd
sion <i.solomon18@hotmail.com>
2025-06-23 23:41:27 +0300

    FEAT: ugly fix for drawing order

--- core/Neuron/Neuron.cpp
+++ core/Neuron/Neuron.cpp
@@ -1,5 +1,5 @@
 #include "Neuron.hpp"
-
+#include <iostream>
 void Neuron::SetColor(const NeuronColor neuronColor)
 {
     Circle::SetColor(neuronColor.fillColor, neuronColor.fillColor);
@@ -11,8 +11,11 @@ void Neuron::Update()
     trValue << std::fixed << std::setprecision(2) << m_value;
     m_text = trValue.str();
     Circle::Update();
+    Canvas::getInstance().DrawText(m_text, static_cast<int>(Circle::GetPosition().x) - 8, static_cast<int>(Circle::GetPosition().y) - 3, 12, raylib::Color::Black());
 };
 
 void Neuron::Draw() const {
-    // Canvas::getInstance().DrawText(m_text, static_cast<int>(Circle::GetPosition().x) - 8, static_cast<int>(Circle::GetPosition().y) - 3, 300, raylib::Color::Black());
+    // TODO: fix this shit workaround
+    //       this helps with drawing order
+    //       (so text isn't overwritten by the actual neuron cicle)
 };
